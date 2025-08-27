0a7293d7581c4b97d8e5d35c727c22dc30e4a975
RuihanCao <116044324+RuihanCao@users.noreply.github.com>
2025-08-27 13:25:13 -0400

    fix sabertooth tiger

--- src/app/classes/pets/star/tier-6/sabertooth-tiger.class.ts
+++ src/app/classes/pets/star/tier-6/sabertooth-tiger.class.ts
@@ -24,8 +24,8 @@ export class SabertoothTiger extends Pet {
         let totalHurt = this.hurtThisBattle;
         if (totalHurt > 0) {
             for (let i = 0; i < this.level; i++) { // 1/2/3 Mammoths based on level
-                let mammothAttack = 4 + (2 * totalHurt);
-                let mammothHealth = 12 + (3 * totalHurt);
+                let mammothAttack = 2 * totalHurt;
+                let mammothHealth = 3 * totalHurt;
                 
                 let mammoth = this.petService.createPet({
                     name: "Mammoth",
