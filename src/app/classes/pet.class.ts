27f0785c83188d04a5e09301693625680a138334
pecies <patboss63@gmail.com>
2025-09-06 16:24:51 -0700

    only try to access this.equipment.name if this.equipment is not null

--- src/app/classes/pet.class.ts
+++ src/app/classes/pet.class.ts
@@ -1296,7 +1296,7 @@ export abstract class Pet {
             message += ' (Albatross)'
         }
 
-        if (equipment) {
+        if (equipment && this.equipment) {
             message += ` (${this.equipment.name})`
         }
 
