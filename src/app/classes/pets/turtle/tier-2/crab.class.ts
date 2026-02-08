c7a8d53e84639edc5f647e2652576b7d43eb6330
Sai Vaka <saivaka07@gmail.com>
2026-02-08 13:37:25 -0500

    Crab rounding fix

--- src/app/classes/pets/turtle/tier-2/crab.class.ts
+++ src/app/classes/pets/turtle/tier-2/crab.class.ts
@@ -64,7 +64,7 @@ export class CrabAbility extends Ability {
       return;
     }
 
-    let gainAmmt = Math.floor(
+    let gainAmmt = Math.round(
       highestHealthResp.pet.health * (0.25 * this.level),
     );
     let selfTargetResp = owner.parent.getThis(owner);
